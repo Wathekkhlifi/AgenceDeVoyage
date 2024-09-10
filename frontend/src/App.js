0e41a80dc0f93aa27785c93a128a7f66ed95a209
Wathek <khlifi.wathek@gmail.com>
2024-09-10 19:29:12 +0100

    - added express server - added axios to react front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [test, setTest] = useState("");
+
+  useEffect(() => {
+    const test = async () => {
+      // Making a GET request
+      axios
+        .get("http://localhost:8080/users")
+        .then((response) => {
+          // Handle success
+          console.log(response.data);
+          setTest(response.data);
+        })
+        .catch((error) => {
+          // Handle error
+          console.error("Error fetching data:", error);
+        });
+    };
+
+    test();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{test}</p>
     </div>
   );
 }
